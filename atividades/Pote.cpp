97df191253c53a0b2befee3b9d2510e0c94d44a0
petrusfilho2001 <petrucio-filho@hotmail.com>
2020-03-16 23:09:43 -0300

    atualizado

--- atividades/Pote.cpp
+++ atividades/Pote.cpp
@@ -5,55 +5,74 @@
 
 
 using namespace std;
-struct Pote{
-    string tamanho;//pequeno medio ou grande
-    string conteudo_do_pote;
+struct Comida {
+    string tipo;
     int qtd_do_conteudo;
+    Comida(string tipo = "", int qtd_do_conteudo=0){
+        this->tipo = tipo;
+        this->qtd_do_conteudo=qtd_do_conteudo;
+        cout << this->tipo << " comprado\n"; 
+    }
+    ~Comida(){
+        cout << this->tipo << " acabou\n";
+    }
+};
+struct Pote{
+    Comida *alimento {nullptr};
+    string tamanho;//pequeno medio ou grande
+    
     int qtd_max;
-    Pote(string tamanho="", string conteudo=""){
+    Pote(string tamanho = ""){
         this->tamanho=tamanho;
-        this->conteudo_do_pote=conteudo;
         if(this->tamanho=="pequeno") this->qtd_max=2;
         else if(this->tamanho=="medio") this->qtd_max=4;
         else if(this->tamanho=="grande") this->qtd_max=10;
     }
+    void comprar_comida(Comida * alimento){
+        if(this->alimento != nullptr){
+            cout << "Já tem um tipo de comida dentro do pote, apenas compre mais dessa comida\n";
+        }else{
+            this->alimento=alimento;
+        }
+    }
     void encher_pote(int quantidade){
         if(this->qtd_max==0) cout << "Você não tem pote, compre um pote\n";
-        else if(this->qtd_do_conteudo==this->qtd_max){
+        else if (alimento==nullptr) cout << "Compre algum alimento para colocar no pote";
+        else if(alimento->qtd_do_conteudo==this->qtd_max){
                 cout << "Não cabe mais nada no seu pote\n";
             }
-        else if((quantidade>this->qtd_max || quantidade+this->qtd_do_conteudo>this->qtd_max) && this->qtd_do_conteudo<this->qtd_max){
-                this->qtd_do_conteudo=this->qtd_max;
-                cout << "Seu pote de "<< this->conteudo_do_pote<<" encheu que derramou\n";
+        else if((quantidade>this->qtd_max || quantidade+alimento->qtd_do_conteudo>this->qtd_max) && alimento->qtd_do_conteudo<this->qtd_max){
+                alimento->qtd_do_conteudo=this->qtd_max;
+                cout << "Seu pote de "<< alimento->tipo <<" encheu que derramou\n";
             }
-        else if((quantidade==this->qtd_max && this->qtd_do_conteudo==0) || quantidade+this->qtd_do_conteudo==this->qtd_max){
-                this->qtd_do_conteudo=this->qtd_max;
-                cout << "Você acabou de encher seu pote de " << this->conteudo_do_pote << "\n";
+        else if((quantidade==this->qtd_max && alimento->qtd_do_conteudo==0) || quantidade+alimento->qtd_do_conteudo==this->qtd_max){
+                alimento->qtd_do_conteudo=this->qtd_max;
+                cout << "Você acabou de encher seu pote de " << alimento->tipo << "\n";
             }
-        else if(quantidade+this->qtd_do_conteudo<this->qtd_max){
-                this->qtd_do_conteudo+=quantidade;
-                cout << "Ainda tem espaço para colocar mais "<< this->conteudo_do_pote<< endl;
+        else if(quantidade+alimento->qtd_do_conteudo<this->qtd_max){
+                alimento->qtd_do_conteudo+=quantidade;
+                cout << "Ainda tem espaço para colocar mais "<< alimento->tipo<< endl;
             }
     }
     void usar_conteudo_do_pote(int quantidade){
         if(this->qtd_max==0) cout << "Você não tem pote, compre um pote\n";
-        else if(this->qtd_do_conteudo==0){ 
+        else if(alimento->qtd_do_conteudo==0){ 
             cout << "O pote está vazio\n";
         }    
-        else if((quantidade==this->qtd_max && this->qtd_do_conteudo==this->qtd_max) || this->qtd_do_conteudo-quantidade==0){
+        else if((quantidade==this->qtd_max && alimento->qtd_do_conteudo==this->qtd_max) || alimento->qtd_do_conteudo==0){
             cout<<"Você secou o pote, encha ele depois\n";
-            this->qtd_do_conteudo-=quantidade;
+            alimento->qtd_do_conteudo-=quantidade;
             
         }        
-        else if(this->qtd_do_conteudo>quantidade && quantidade<this->qtd_max){
-            this->qtd_do_conteudo-=quantidade;
-            cout << "O pote agora tem "<< this->qtd_do_conteudo<<"kg de "<<this->conteudo_do_pote<< endl;
+        else if(alimento->qtd_do_conteudo>quantidade && quantidade<this->qtd_max){
+            alimento->qtd_do_conteudo-=quantidade;
+            cout << "O pote agora tem "<< alimento->qtd_do_conteudo<<"kg de "<<this->alimento->tipo<< endl;
             
         }else if(quantidade>this->qtd_max){
             cout << "O seu pote é " << this->tamanho<< " para retirar essa quantidade compre um pote maior\n";
         }
-        else if(this->qtd_do_conteudo<quantidade && quantidade<=this->qtd_max){
-            cout << "O pote não possui toda essa quantidade de "<< this->conteudo_do_pote << " encha mais o pote\n";
+        else if(alimento->qtd_do_conteudo<quantidade && quantidade<=this->qtd_max){
+            cout << "O pote não possui toda essa quantidade de "<< this->alimento->tipo << " encha mais o pote\n";
             
         }    
     }
@@ -63,7 +82,7 @@ int main(){
     system("clear||cls");
     while (true){
         system("clear||cls");
-        cout << "comprar (pequeno, medio ou grande)(conteudo) encher (quantidade em kg) usar(quantidade) end" << endl;
+        cout << "comprar(pote= pequeno, medio ou grande) comida(tipo e quantidade) encher (quantidade em kg) usar(quantidade) end" << endl;
         cout << "\n";
         string line;
         getline(cin,line);
@@ -75,11 +94,18 @@ int main(){
             break;
         }
         else if(cmd == "comprar"){
-            string tamanho, conteudo;
+            string tamanho;
             ss>> tamanho;
-            ss>>conteudo;
-            pote=new Pote(tamanho, conteudo);
+            pote=new Pote(tamanho);
             cout<<"Pote comprado\n";
+            getchar();
+        }
+        else if(cmd == "comida"){
+            string tipo;
+            int qtd;
+            ss >> tipo >> qtd;
+            pote->comprar_comida(new Comida(tipo, qtd));
+            cout<<"Pacote de "<< pote->alimento->tipo<<" comprado\n";
             getchar();
         }  
         else if(cmd == "encher"){
